f50c30bc5799d0915e4c4b8d4b834b39af0afb74
Strider White <strider.white@pderas.com>
2022-05-15 16:31:37 -0600

    added service provider skeleton

---
+++ src/TemplarServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pderas\Templar;
+
+use Illuminate\Support\ServiceProvider;
+
+class TemplarServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
